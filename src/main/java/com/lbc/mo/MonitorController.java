27bcb309cd63aa898ef4a8c8935b68f337922e94
changlb1 <changlb1@lenovo.com>
2019-06-15 16:05:59 +0800

    fix netty

--- src/main/java/com/lbc/mo/MonitorController.java
+++ src/main/java/com/lbc/mo/MonitorController.java
@@ -4,6 +4,7 @@ import com.lbc.mo.bean.StreamMessageFactory;
 import com.lbc.mo.bean.StreamType;
 import com.lbc.mo.netty.NettyClient;
 import com.lbc.mo.netty.StreamService;
+import com.lbc.mo.service.TestService;
 import io.grpc.netty.shaded.io.netty.buffer.ByteBuf;
 import io.grpc.netty.shaded.io.netty.buffer.Unpooled;
 import io.grpc.netty.shaded.io.netty.channel.Channel;
@@ -20,32 +21,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class MonitorController {
     private static final Log LOG = LogFactory.getLog(MonitorController.class);
     @Autowired
-    NettyClient nettyClient;
+    TestService testService;
     @Autowired
     EhCacheCacheManager ehCacheCacheManager;
 
 
     @RequestMapping(value = "/manage", method = RequestMethod.POST)
-    public String manageGpuLable(@RequestParam String sDay, @RequestParam String eDay, @RequestParam String v100TOP) {
+    public String manageGpuLable(@RequestParam String sDay, @RequestParam String eDay) {
         return "done";
     }
 
 
     @RequestMapping(value = "/testNetty", method = RequestMethod.GET)
     public void manageGpuLable() {
-        try {
-            String sendMsg = "send msg";
-            byte[] msgBody = StreamMessageFactory.factory((byte) 0x01, StreamType.LOG.getValue(), sendMsg.getBytes());
-            Channel channel = null;
-            channel = nettyClient.connect("localhost", StreamService.getPort(), "testContainerName");
-            if (!channel.isActive()) {
-                nettyClient.cleanCon("testContainerName");
-                channel = nettyClient.connect("localhost", StreamService.getPort(), "testContainerName");
-            }
-            ByteBuf meg = Unpooled.wrappedBuffer(msgBody);
-            channel.writeAndFlush(meg);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        testService.conNetty();
     }
 }
--- src/main/java/com/lbc/mo/service/impl/TestServiceImpl.java
+++ src/main/java/com/lbc/mo/service/impl/TestServiceImpl.java
@@ -2,9 +2,17 @@ package com.lbc.mo.service.impl;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lbc.mo.bean.GroupUsers;
+import com.lbc.mo.bean.StreamMessageFactory;
+import com.lbc.mo.bean.StreamType;
+import com.lbc.mo.netty.NettyClient;
+import com.lbc.mo.netty.StreamService;
 import com.lbc.mo.service.TestService;
 import com.lbc.mo.utils.RedisUtil;
+import io.grpc.netty.shaded.io.netty.buffer.ByteBuf;
+import io.grpc.netty.shaded.io.netty.buffer.Unpooled;
+import io.grpc.netty.shaded.io.netty.channel.Channel;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import redis.clients.jedis.Jedis;
@@ -13,6 +21,8 @@ import java.io.IOException;
 @Service
 public class TestServiceImpl implements TestService {
 
+    @Autowired
+    NettyClient nettyClient;
 
     @Override
     @Cacheable(value = "redisUserInfo", key = "#user")
@@ -44,6 +54,24 @@ public class TestServiceImpl implements TestService {
             return "lb";
         }else {
             return null;
+        }
+    }
+
+    @Override
+    public void conNetty() {
+        try {
+            String sendMsg = "send msg";
+            byte[] msgBody = StreamMessageFactory.factory((byte) 0x01, StreamType.LOG.getValue(), sendMsg.getBytes());
+            Channel channel = null;
+            channel = nettyClient.connect("localhost", StreamService.getPort(), "testContainerName");
+            if (!channel.isActive()) {
+                nettyClient.cleanCon("testContainerName");
+                channel = nettyClient.connect("localhost", StreamService.getPort(), "testContainerName");
+            }
+            ByteBuf meg = Unpooled.wrappedBuffer(msgBody);
+            channel.writeAndFlush(meg);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
